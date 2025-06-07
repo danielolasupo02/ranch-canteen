f4b034dbaf26c99f46ca5fa7b235bcffbfaa5ddf
danielolasupo02 <danielolasupo02@gmail.com>
2025-06-07 23:33:40 +0100

    chore: initial Spring Boot project setup with dependencies

---
+++ backend/src/main/java/ng/unionbank/ranchcanteen/ranch_canteen/RanchCanteenApplication.java
@@ -0,0 +1,13 @@
+package ng.unionbank.ranchcanteen.ranch_canteen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RanchCanteenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RanchCanteenApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ng/unionbank/ranchcanteen/ranch_canteen/RanchCanteenApplicationTests.java
@@ -0,0 +1,13 @@
+package ng.unionbank.ranchcanteen.ranch_canteen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RanchCanteenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
